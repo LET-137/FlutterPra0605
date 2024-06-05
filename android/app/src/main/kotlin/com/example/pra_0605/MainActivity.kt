ae7ed8e9aa6c6bf18672cd427b5e5db12daa92e7
津本拓也 <tsumototakuya@LETM3.local>
2024-06-05 18:34:44 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/pra_0605/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pra_0605
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MaterialApp(
+    home: FirstScreen(),
+  ));
+}
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  int _number = 0;
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('firstScreen'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              'number = $_number',
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                final newNumber = await Navigator.of(context).push<int>(
+                  MaterialPageRoute(
+                    builder: (_) => SecondScreen(number: _number,text: _controller.text,),
+                  ),
+                );
+                setState(() {
+                  if (newNumber != null) {
+                    _number = newNumber;
+                  }
+                });
+              }, 
+             child: const Text('secondへ'),
+            ),
+            TextField(
+              controller: _controller,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'enter'
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+    
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key, required this.number, required this.text});
+
+  final int number;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SecondScreen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop(number + 1);
+              }, 
+              child: const Text('Increment'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop(number - 1);
+              },
+              child: const Text('Decrement')),
+            Text(
+              text,
+              style: const TextStyle(fontSize: 20),),
+          ],
+        ),
+      ),
+    );
+  }
+}
